be875b24e8abd3dff31486bf32776feef3ed9bb9
erikbrandondigital <erikbrandon1@gmail.com>
2025-03-28 18:09:01 -0400

    Added Claude AI to ZSH Web-Search Plugin - In `.config/zsh/ohmyzsh.zsh`, added a `ZSH_WEB_SEARCH_ENGINES`   environment variable which enables the ability to open Claude AI in a   web browser with a pre-filled input field containing a query string   passed from the Oh-My-ZSH web-search plugin.
    Signed-off-by: erikbrandondigital <erikbrandon1@gmail.com>

--- .config/zsh/ohmyzsh.zsh
+++ .config/zsh/ohmyzsh.zsh
@@ -7,6 +7,7 @@ export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST # Customize ZSH Completion Cache
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion) # Customize ZSH_AUTOSUGGEST_STRATEGY
 export UPDATE_ZSH_DAYS=14 # Have ohmyzsh-full-autoupdate plugin check for updates every 14 days.
 export ZSH_TMUX_AUTOSTART=true # Autostart TMUX in ZSH using Oh My ZSH plugin.
+export ZSH_WEB_SEARCH_ENGINES=(claude "https://claude.ai/new?q=") # Add Claude to Oh-My-ZSH web-search plugin
 
 source $ZSH/oh-my-zsh.sh
 
