c987b0932835b896146b0532b28eca25519323d7
erikbrandondigital <erikbrandon1@gmail.com>
2025-03-28 21:40:53 -0400

    Changed nvim-tree gitignore & dotfiles Defaults - In `.config/nvim/lua/plugins/nvim-tree.lua`, added two filter options   to force nvim-tree to display gitignored and hidden files by default   since I was always toggling the setting when opening projects in   neovim.
    Signed-off-by: erikbrandondigital <erikbrandon1@gmail.com>

---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  opts = {
+    filters = {
+      git_ignored = false, -- Show .gitignored files by default
+      dotfiles = false, -- Show dot/hidden files by default
+    },
+  },
+}
