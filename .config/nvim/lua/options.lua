6e95324c3fc8b71093fae14fcccff9a74e6f88c0
Erik Brandon <erikbrandon1@gmail.com>
2025-08-09 12:55:10 -0400

    Enable Relative Number by Default in Neovim - In `.config/nvim/lazy-lock.json`, enable relative line numbers by   default.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -3,7 +3,5 @@ require "nvchad.options"
 -- Prepend mise shims to PATH
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
 
--- add yours here!
-
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.relativenumber = true -- Enable relative line numbers
